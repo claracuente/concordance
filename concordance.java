ddfc98d72a855bac69a6cf6f02ccb70e97e160ba
Colby <colby.laracuente@uconn.edu>
2021-07-02 10:57:13 -0400

    third commit

--- concordance.java
+++ concordance.java
@@ -1,6 +1,17 @@
-public class concordance {
-    public static void main(String[] args){
+import java.io.*;
+public class Concordance { 
+    public Concordance() throws IOException{
+        BufferedReader s = new BufferedReader(new FileReader("text.txt"));
+        String line = s.readLine();
+        String[] words = line.split(" ");
+        System.out.println("");
+        for (String a : words){
+            System.out.println(a);
+        }
+        s.close();
 
     }
-
+    public static void main(String[] args) throws IOException{
+    new Concordance();
+    }
 }
