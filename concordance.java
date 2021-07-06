8914d44fe3acfa48502e4e00f5074914d9770ac0
Colby <colby.laracuente@uconn.edu>
2021-07-06 13:33:46 -0400

    fourth commit

--- concordance.java
+++ concordance.java
@@ -1,17 +1,36 @@
 import java.io.*;
-public class Concordance { 
-    public Concordance() throws IOException{
+import java.util.*;
+
+public class concordance { 
+    Hashtable<String, Integer> table = new Hashtable<String, Integer>();
+    String key;
+    Integer temp_int = 0;
+
+    public concordance() throws IOException{
         BufferedReader s = new BufferedReader(new FileReader("text.txt"));
         String line = s.readLine();
-        String[] words = line.split(" ");
-        System.out.println("");
-        for (String a : words){
-            System.out.println(a);
+        while (line != null){
+            String[] words = line.split(" ");
+            for (String a : words){
+                if(table.containsKey(a)){
+                    temp_int = table.get(a);
+                    table.put(a, temp_int+1);
+                }else{
+                    table.put(a, 1);
+                }
+            }
+            line = s.readLine();
+        }
+        Enumeration<String> e = table.keys();
+        while (e.hasMoreElements()){
+            
+            key = e.nextElement();
+            System.out.println(key + ", " + table.get(key).toString());
         }
         s.close();
 
     }
     public static void main(String[] args) throws IOException{
-    new Concordance();
+    new concordance();
     }
 }
